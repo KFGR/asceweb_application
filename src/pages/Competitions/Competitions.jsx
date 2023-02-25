3dd626649080e69847edb1518a1fd9c01c4cf89a
Ale-Carrera-97 <ale97carrera@gmail.com>
2023-02-24 20:48:10 -0400

    Small fixes to list display - still needs work.

--- src/pages/Competitions/Competitions.jsx
+++ src/pages/Competitions/Competitions.jsx
@@ -9,33 +9,40 @@ function template() {
       <h2>There are several ASCE competitions.</h2>
       <h3>All of the following regional competitions will be held from March 23, 2023 to March 25, 2023.</h3>
  
-      <h4>Of these competitions, the most important are:</h4>
-      <ol>
-      <li>1. Concrete Canoe</li>
-      <li>2. Steel Bridge</li>
-      <li>3. Timber Strong</li>
-      <li>4. Sustainable Solutions</li>
-      <li>5. Innovation Contest</li>
-      <li>6. Surveying</li> 
-      <li>7. Construction Institute</li>
-      </ol>
       
- 
-      <h5>The rest are:</h5>
-      <ol>
-      <li>1. Concrete Bridge</li>
-      <li>2. Traffic Control</li>
-      <li>3. Geo-Wall</li>
-      <li>4. Muddy Waters</li>
-      <li>5. Cncrete Cornhole</li>
-      <li>6. Plans Reading</li>
-      <li>7. Professional Paper</li>
-      <li>8. T-Shirt Contest</li>
-      <li>9. Mystery Competition</li>
+
+      <h4>Of these competitions, the most important are:</h4>
+      <div>     
+      <ol className = 'firstCompetitionsList'>
+      <li>Concrete Canoe</li>
+      <li>Steel Bridge</li>
+      <li>Timber Strong</li>
+      <li>Sustainable Solutions</li>
+      <li>Innovation Contest</li>
+      <li>Surveying</li> 
+      <li>Construction Institute</li>
       </ol>
+      </div>
       </section>
+
+      <section>
+      <div>
+      <h5>The rest are:</h5>
+      <ol className="secondCompetitionList">
+      <li>Concrete Bridge</li>
+      <li>Traffic Control</li>
+      <li>Geo-Wall</li>
+      <li>Muddy Waters</li>
+      <li>Concrete Cornhole</li>
+      <li>Plans Reading</li>
+      <li>Professional Paper</li>
+      <li>T-Shirt Contest</li>
+      <li>Mystery Competition</li>
+      </ol>
+      </div>
+      </section>
+
     </div>
   );
 };
-
 export default template;
