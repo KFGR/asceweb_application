8f2519590d73a52f3fe1e88fd1dfd7ca8d6094b8
KFGR <figueroa.8254@gmail.com>
2023-03-23 09:04:52 -0400

    changed colors of links and names

--- src/components/NavegationBar/NavegationBar.jsx
+++ src/components/NavegationBar/NavegationBar.jsx
@@ -9,13 +9,13 @@ function template() {
     <div className="navegation-bar navegation-bar-menu">
       <Navbar className='navbar navbar-expand-lg' expand='lg'>
         <Navbar.Toggle aria-controls='basic-navbar' style={{color:'#fbb71a', backgroundColor:'#144a9a'}}/>
-          <Navbar.Collapse  id='basic-navbar'>
+          <Navbar.Collapse  id='basic-navbar' style={{color:'#fbb71a'}}>
             <ul className='navbar-nav nav-masthead margin-left-auto mb-2 mb-lg-0'>
-            <Nav.Link as={Link} to="/Home" className='nav-link py-1' aria-current="page" ><li style={{color:'black', fontSize:'150%'}}>Home</li></Nav.Link>
-            <Nav.Link as={Link} to="/Team" className='nav-link py-1' aria-current="page" ><li style={{color:'black', fontSize:'150%'}}>Team</li></Nav.Link>
-            <Nav.Link as={Link} to="/Competitions" className='nav-link py-1' aria-current="page" ><li style={{color:'black', fontSize:'150%'}}>Competitions</li></Nav.Link>
-            <Nav.Link as={Link} to="/Contacts" className='nav-link py-1' aria-current="page" ><li style={{color:'black', fontSize:'150%'}}>Contacts</li></Nav.Link>
-            <Nav.Link as={Link} to="/Join" className='nav-link py-1' aria-current="page" ><li style={{color:'black', fontSize:'150%'}}>Join</li></Nav.Link>
+            <Nav.Link as={Link} to="/Home" className='nav-link py-1' aria-current="page" ><li style={{color:'white', fontSize:'2rem'}}>Home</li></Nav.Link>
+            <Nav.Link as={Link} to="/Team" className='nav-link py-1' aria-current="page" ><li style={{color:'white', fontSize:'2rem'}}>Team</li></Nav.Link>
+            <Nav.Link as={Link} to="/Competitions" className='nav-link py-1' aria-current="page" ><li style={{color:'white', fontSize:'2rem'}}>Competitions</li></Nav.Link>
+            <Nav.Link as={Link} to="/Contacts" className='nav-link py-1' aria-current="page" ><li style={{color:'white', fontSize:'2rem'}}>Contacts</li></Nav.Link>
+            <Nav.Link as={Link} to="/Join" className='nav-link py-1' aria-current="page" ><li style={{color:'white', fontSize:'2rem'}}>Join</li></Nav.Link>
  
               {/* <Nav.Link  className='nav-link py-1'  aria-current="page" href='#Home'><li style={{color:'black', fontSize:'150%'}}>Home</li></Nav.Link> style={{color:'black', fontSize:'150%'}} */}
               {/* <Nav.Link  className='nav-link py-1'  aria-current="page" href='#Team'><li style={{color:'black', fontSize:'150%'}}>Team</li></Nav.Link> style={{color:'black', fontSize:'150%'}} */}
