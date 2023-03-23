eec1e9ad4ef2b83f38e0b8e04976ce8d35ae65a6
KFGR <figueroa.8254@gmail.com>
2023-03-23 09:02:38 -0400

    implemented react Icon tag in line 12

--- src/components/AppLogo/AppLogo.jsx
+++ src/components/AppLogo/AppLogo.jsx
@@ -1,15 +1,21 @@
 import "./AppLogo.css";
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
+import Icon from "./logo";
 
 function template() {
   return (
     <div className="app-logo">
-      <div className='col-md-3'>
-        <Navbar.Brand href='#home'><img src={require("../../assets/Brand/Logo.png")} alt='img-fluid'/></Navbar.Brand>
+      {/* <div className='col-md-4'> col-md-3 */}
+        {/* <Navbar.Brand href='#home'><img src={require("../../assets/Brand/Logo.png")} alt='img-fluid'/></Navbar.Brand> */}
+        <div className="svg-container">
+         <Navbar.Brand href='/Home'><svg><Icon/></svg></Navbar.Brand>
+        </div>
       </div>
-    </div>
+    // </div>
   );
 };
 
 export default template;
+
+
