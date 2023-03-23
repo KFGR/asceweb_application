f88a5812e1d8b2758f3f20a2af9b9d0da9a3f660
KFGR <figueroa.8254@gmail.com>
2023-03-23 09:01:21 -0400

    changed lines 12-15 and 22-25

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -9,17 +9,17 @@ function template() {
     <section id="header" className="header">
     <div className="container-fluid text-center">
       <div className="header-area header-area--absolute">
-        <div className="header-top-bar-info d-lg-block">
+        {/* <div className="header-top-bar-info d-lg-block">
           <div className="container-fluid py-2 header-yllw"/> 
           <div className="container-fluid py-3 header-ble"/>        
-        </div>
+        </div> */}
         <div className='header-bottom-wrap header-sticky'>
           <div className='container-fluid' style={{boxShadow:'none'}}>
             <div className='row'>
               <div className='col-lg-12'>
                 <div className='header position-relative'>
                   <div className='header-right'>
-                    <div className='container-fluid'>
+                    <div className='container-fluid header-containerr'>
                       <AppLogo></AppLogo>
                       <NavegationBar></NavegationBar>
                     </div>
@@ -31,7 +31,6 @@ function template() {
         </div>
       </div>
     </div>
-  
     </section>
   );
 };
