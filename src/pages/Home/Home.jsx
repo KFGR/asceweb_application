c06b2e3842afe6b05c1b923363e353f90c3902a6
KFGR <figueroa.8254@gmail.com>
2023-03-23 09:08:17 -0400

    changed Home structure, implemented ContentCard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,29 @@
 import "./Home.css";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHandshake, faUsers, faTrowelBricks } from "@fortawesome/free-solid-svg-icons";
+import { faHandshake, faUsers} from "@fortawesome/free-solid-svg-icons";
 import { Image } from "react-bootstrap";
-
+// import HeaderHero from './HeaderHero';
+import ContentCard from '../../components/ContentCard';
+import Award from '../../assets/Brand/Award.png';
 
 function template (){
+  console.log(Award);
+  console.log(typeof Award);
   return (
     <div className="home">
+      <ContentCard
+        className = "d-block mx-lg-auto img-fluid shadow-box-right-sm"
+        imageSrc = {Award}
+        imageAlt = "notabout"
+        title = "About Us"
+        paragraph = "Quickly design and customize responsive mobile-first sites with Bootstrap, the worlds most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins."
+      
+      />
+      {/* <HeaderHero/> */}
       {/* <AppHeader></AppHeader> */}
 
-      <section id="intro">
+      {/* <section id="intro">
         <div className="container-fluid justify-content-center intro-content" style={{marginBottom: '1%'}}>
           <div className="container-content ">
             <div className="box">
@@ -29,84 +42,104 @@ function template (){
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
+
+      {/* <section id="intro">
+          <div className="hero-image">
+            <div className="hero-text">
+              <div className="font-size:50px">I am Joe Doe</div>
+              <p>And I'm a Photographer</p>
+              <button>Hire me</button>
+            </div>
+          </div>
+      </section> */}
+
+      {/* <section id="intro">
+        <div className="hero-containerr">
+          <div className="hero-backgrond-image">
+            <h1>Whatever hero</h1>
+            <div className="col-lg-6 hero-content">
+              <p className="lead">algo mas</p>
+            </div>
+          </div>
+        </div>
+      </section> */}
+      
+
+     
 
       {/* Section divider */}
-      <div className="container-fluid py-2 header-yllw"/> 
+      <div className="container-fluid py-2 header-yllw"/>
+      {/* About US section */}
+      <div className="about-us">
+          <div className="container col-xxl-8 px-4 py-5">
+            {/* About Us */}
+            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+              <div className="col-10 col-sm-8 col-lg-6">
+                {/* <img src={require("./bs.png")} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/> */}
+                <Image src={require("../../assets/Brand/members.png")} className="d-block mx-lg-auto img-fluid shadow-box-right-sm" alt=""  />
+                {/* <Image src={require("./bs.png")} className="d-block mx-lg-auto img-fluid shadow-box-right-sm" alt=""  /> */}
 
-      {/* Section for Mission Vission and Values */}
-      <div className="container-fluid  container-content values-content">
-        <div>
-          <h1 className='h1-text-content' id='mission_txt' style={{color:'white', textAlign:'center'}}><strong>About Us</strong></h1>
-        <div className="col">
-         <div className="row" style={{paddingRight:'5%', paddingBottom:'5%'}}>
-          <div className="col-sm-4">
-            <FontAwesomeIcon icon={faTrowelBricks} size="2" style={{color:'red'}}/>
-          </div>
-          <div className="col-sm-8" style={{paddingRight:'5%'}}>
-            <div className="container-fluid container-shadow" style={{backgroundColor:'#144a9a'}}> {/*container-shadow is not working because of container-fluid */}
-              <h2><strong>Foundation</strong></h2>
-              <h4 style={{color: 'white'}}>
-                Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </h4>
-          </div>
-          </div>
-        </div>
+              </div>
+              <div className="col-lg-6">
+                <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>A</span>bout <span>U</span>s</h1>
+                <h4 className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the worlds most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</h4>
+                {/* <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                  <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                  <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                </div> */}
+              </div>
+            </div>
 
-        {/* Vision */}
-        <div className="row" style={{paddingLeft:'5%', paddingBottom:'5%'}}>
-          <div className="col-sm-8" style={{paddingRight:'5%'}}>
-            <div className="container-fluid container-shadow" style={{backgroundColor:'#144a9a'}}> {/*container-shadow is not working because of container-fluid */}
-              <h2><strong>Activities</strong></h2>
-              <h4 style={{color: 'white'}}>
-                Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </h4>
-          </div>
-          </div>
-          <div className="col-sm-4">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1000" height="16" fill="currentColor" className="bi bi-globe-americas globe-icon" viewBox="0 0 16 16">
-              <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
-            </svg>
-          </div>
-        </div>
+            {/* Activities*/}
+            <div className="row flex-lg-row align-items-center g-5 py-5">
+              <div className="col-10 col-sm-8 col-lg-6">
+                {/* <img src={require("./bs.png")} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/> */}
+                <Image src={require("../../assets/Brand/Award.png")} className="d-block mx-lg-auto img-fluid shadow-box-left-sm" alt=""  />
+              </div>
+              <div className="col-lg-6">
+                <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>A</span>ctiviti<span>e</span>s</h1>
+                <h4 className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the worlds most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</h4>
+                {/* <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                  <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                  <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                </div> */}
+              </div>
+            </div>
 
-        {/* Values */}
-        <div className="row" style={{paddingRight:'5%', paddingBottom:'5%'}}>
-          <div className="col-sm-4">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1000" height="16" fill="currentColor" className="bi bi-globe-americas globe-icon" viewBox="0 0 16 16">
-              <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0ZM2.04 4.326c.325 1.329 2.532 2.54 3.717 3.19.48.263.793.434.743.484-.08.08-.162.158-.242.234-.416.396-.787.749-.758 1.266.035.634.618.824 1.214 1.017.577.188 1.168.38 1.286.983.082.417-.075.988-.22 1.52-.215.782-.406 1.48.22 1.48 1.5-.5 3.798-3.186 4-5 .138-1.243-2-2-3.5-2.5-.478-.16-.755.081-.99.284-.172.15-.322.279-.51.216-.445-.148-2.5-2-1.5-2.5.78-.39.952-.171 1.227.182.078.099.163.208.273.318.609.304.662-.132.723-.633.039-.322.081-.671.277-.867.434-.434 1.265-.791 2.028-1.12.712-.306 1.365-.587 1.579-.88A7 7 0 1 1 2.04 4.327Z"/>
-            </svg>
+            {/* Awards */}
+            <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
+              <div className="col-10 col-sm-8 col-lg-6">
+                {/* <img src={require("./bs.png")} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/> */}
+                <Image src={require("../../assets/Brand/Award.png")} className="d-block mx-lg-auto img-fluid shadow-box-right-sm" alt=""  />
+              </div>
+              <div className="col-lg-6">
+                <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>A</span>war<span>d</span>s</h1>
+                <h4 className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the worlds most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</h4>
+                {/* <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                  <button type="button" class="btn btn-primary btn-lg px-4 me-md-2">Primary</button>
+                  <button type="button" class="btn btn-outline-secondary btn-lg px-4">Default</button>
+                </div> */}
+              </div>
+            </div>
+
+            
           </div>
-          <div className="col-sm-8" style={{paddingRight:'5%'}}>
-            <div className="container-fluid container-shadow" style={{backgroundColor:'#144a9a'}}> {/*container-shadow is not working because of container-fluid */}
-              <h2><strong>Awards</strong></h2>
-              <h4 style={{color: 'white'}}>
-                Our mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              </h4>
-          </div>
-          </div>
-        </div>
-        
-        </div>
-        </div>
       </div>
 
-      {/* Section divider */}
-      <div className="container-fluid py-2 header-yllw"/> 
+  
+
+
+       {/* Section divider */}
+       <div className="container-fluid py-2 header-yllw"/> 
 
 
         <div className="container container-values">
           <div>
             <h1 className='h1-text-content' id='mission_txt' style={{color:'black', textAlign:'center'}}><strong>Our Mission</strong></h1>
-            <div className="row">
+            <div className="row ">
               <div className="col-12">
-                <div className="row">
+                <div className="row row-values">
                   <div className="col-lg-6">
                       <div className="grid-item move-up">
                         <div className="ht-box-icon">
@@ -192,11 +225,13 @@ function template (){
       </div>
 
     {/* Section divider */}
+    <div className="container-fluid py-2 header-finite-ble"/> 
     <div className="container-fluid py-2 header-yllw"/> 
+    
     <div className="container container-content">
       <div>
         <h1 className='h1-text-content' id='mission_txt' style={{color:'black', textAlign:'center'}}><strong>Our Sponsors</strong></h1>
-      <Image src={require("./clients.png")} alt="" width={500} height={500} style={{width:'100%', height:'auto'}}/>
+      <Image src={require("../../assets/Brand/clients.png")} alt="" width={500} height={500} style={{width:'100%', height:'auto'}}/>
       {/* <ImageModal show={showModal} onHide={handleCloseModal}/> */}
       </div>
     </div>
