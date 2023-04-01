2362629b9cf9f53eec8b24295c5559f27af53fdc
KFGR <figueroa.8254@gmail.com>
2023-03-31 21:20:01 -0400

    commented line 8-9

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import Team from './pages/Team';
+// import Home from './pages/Home';
+// import Team from './pages/Team';
 // import AppHeader from './components/AppHeader';
 
 // import HeaderHero from './pages/Home/HeaderHero';
