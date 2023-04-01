1b3124c704ed26d47941f27438540de119125791
KFGR <figueroa.8254@gmail.com>
2023-03-31 21:11:24 -0400

    Implemented Bionic Reading methodology

--- src/components/ContentCard/ContentCard.jsx
+++ src/components/ContentCard/ContentCard.jsx
@@ -4,9 +4,9 @@ import { Image } from "react-bootstrap";
 
 function template(props) {
   const {id,className, imageSrc, imageAlt, title, paragraph} = props;
-  console.log(typeof imageSrc);
-  console.log(imageSrc);
-
+  // console.log(typeof imageSrc);
+  // console.log(imageSrc);
+  console.log(title.substring(2,4));
  
   
   return (
@@ -23,7 +23,7 @@ function template(props) {
               </div>
               <div className="col-lg-6">
                 {/* <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>A</span>bout <span>U</span>s</h1> */}
-                <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>{title[0]}</span></h1>
+                <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>{title[0]}</span>{title.substring(1,title.length - 2)}<span>{title.charAt(title.length -2)}</span>{title.charAt(title.length -1)}</h1>
                 {/* <h4 className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the worlds most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</h4> */}
                 <h4 className="lead">{paragraph}</h4>
               </div>
@@ -38,14 +38,15 @@ function template(props) {
                 <div className="col-10 col-sm-8 col-lg-6">
                   {/* <img src={require("./bs.png")} class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy"/> */}
                   {/* <Image src={require("../../assets/Brand/members.png")} className="d-block mx-lg-auto img-fluid shadow-box-right-sm" alt=""  /> */}
-                  <Image src={imageSrc} alt={imageAlt} className={className} />
+                  
+                  {/* <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>A</span>bout <span>U</span>s</h1> */}
+                  <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>{title[0]}</span>{title.substring(1,title.length - 2)}<span>{title.charAt(title.length -2)}</span>{title.charAt(title.length -1)}</h1>
+                  {/* <h4 className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the worlds most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</h4> */}
+                  <h4 className="lead">{paragraph}</h4>
                   {/* <Image src={require ('../../assets/Brand/members.png')}/> */}
                 </div>
                 <div className="col-lg-6">
-                  {/* <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>A</span>bout <span>U</span>s</h1> */}
-                  <h1 className="display-5 fw-bold lh-1 mb-3 h1-text-content"><span>{title[0]}</span></h1>
-                  {/* <h4 className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the worlds most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</h4> */}
-                  <h4 className="lead">{paragraph}</h4>
+                <Image src={imageSrc} alt={imageAlt} className={className} />
                 </div>
               </div>
             </div>
