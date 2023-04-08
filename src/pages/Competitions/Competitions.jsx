bf6ed0aa6b9631059faf7c504a8b5895ce0e7597
Ale-Carrera-97 <ale97carrera@gmail.com>
2023-04-08 04:09:42 -0400

    Cards for top 7-Raw- Needs click&images

--- src/pages/Competitions/Competitions.jsx
+++ src/pages/Competitions/Competitions.jsx
@@ -2,94 +2,129 @@ import "./Competitions.css";
 import React from "react";
 
 
-function Template() { 
+/*Important Competitions: *Concrete Canoe, Steele Bridge, Timber Strong, Sustainable Solutions, Innovation Contest, Surveying, Construction Institute*/
+
+/*Other competitions: Concrete Bridge, Traffic Control, Geo-Wall, Plans Reading, Muddy Waters, Concrete Cornhole, Professional Paper, T-shirt Contest, Mystery Competition */
+
+function template() { 
   return(
+    <div className="Competitions">
+      <h1>Competitions</h1>
+      <p>These are the top ASCE Competitions, celebrated annually.</p>
+      
+      <div class="container">
+        <div class = "row">
 
-  <div className="Competitions">
-      <section>
-      <h1 className = 'classHeaderOne'>Competitions</h1> {/*css class for page title is classHeaderOne and has the name "title"*/}
-      <h2>There are several ASCE competitions.</h2>
-      </section>
-      <h3>All of the following regional competitions will be held from March 23, 2023 to March 25, 2023.</h3>
-      
-      
-      <section>
-      <div>
-      <ol className = 'firstCompetitionsList'>
-      <h4>Of these competitions, the most important are:</h4>
-      <li>Concrete Canoe</li>
-      <li>Steel Bridge</li>
-      <li>Timber Strong</li>
-      <li>Sustainable Solutions</li>
-      <li>Innovation Contest</li>
-      <li>Surveying</li> 
-      <li>Construction Institute</li>
-      </ol>
-      </div>
-      <div>
-      <ol className="secondCompetitionList">
-      <h5>The rest are:</h5>
-      <li>Concrete Bridge</li>
-      <li>Traffic Control</li>
-      <li>Geo-Wall</li>
-      <li>Muddy Waters</li>
-      <li>Concrete Cornhole</li>
-      <li>Plans Reading</li>
-      <li>Professional Paper</li>
-      <li>T-Shirt Contest</li>
-      <li>Mystery Competition</li>
-      </ol>
-      </div>
-      </section>
+          {/*Concrete Canoe */}
+          <div class = "col-12 col-lg-3">
+            <div class ="card text-center h-100 mb-2">
+              <img src ="..." alt="..." class="card-img-top"/>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex flex-row align-items-center">
+                  <div class = "card-body">
+                    <h2>Concrete Canoe</h2>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+          </div>
 
-      <section>
-      
-      </section>
+          {/*Steele Bridge */}
+          <div class = "col-12 col-lg-3">
+            <div class ="card text-center h-100 mb-2">
+              <img src ="..." alt="..." class="card-img-top"/>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex flex-row align-items-center">
+                  <div class = "card-body">
+                    <h3>Steele Bridge</h3>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+          </div>
+
+          {/*Timber Strong */}
+          <div class = "col-12 col-lg-3">
+            <div class ="card text-center p-3 mb-2 h-100 mb-3">
+              <img src ="..." alt="..." class="card-img-top"/>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex flex-row align-items-center">
+                  <div class = "card-body">
+                    <h4>Timber Strong</h4>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+          </div>
+
+          {/*Sustainable Solution */}
+          <div class = "col-12 col-lg-3">
+            <div class ="card text-center p-3 mb-2 h-100 mb-3">
+              <img src ="..." alt="..." class="card-img-top"/>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex flex-row align-items-center">
+                  <div class = "card-body">
+                    <h5>Sustainable Solutions</h5>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+          </div>
+
+          {/*Innovation Contest */}
+          <div class = "col-12 col-md-5 col-lg-4">
+            <div class ="card text-center  mt-2">
+              <img src ="..." alt="..." class="card-img-top"/>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex flex-row align-items-center">
+                  <div class = "card-body">
+                    <h6>Innovation Contest</h6>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+          </div>
+
+          {/*Surveying */}
+          <div class = "col-12 col-md-5 col-lg-4">
+            <div class ="card text-center  mt-2">
+              <img src ="..." alt="..." class="card-img-top"/>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex flex-row align-items-center">
+                  <div class = "card-body">
+                    <h7>Surveying</h7>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+          </div>
+
+          {/*Construction Institure */}
+          <div class = "col-12 col-md-5 col-lg-4">
+            <div class ="card text-center p-2 mt-2">
+              <img src ="..." alt="..." class="card-img-top"/>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex flex-row align-items-center">
+                  <div class = "card-body">
+                    <h8>Construction Institute</h8>
+                  </div>
+                </div>
+              </div>
+            
+            </div>
+          </div>
+
+        </div>
+      </div>
 
     </div>
+
   );
-  };
-export default Template; //changed Template to App
-
-/*<div className="Competitions">
-      <section>
-      <h1 className = 'classHeaderOne'>Competitions</h1> {/*css class for page title is classHeaderOne and has the name "title"*//*}
-      <h2>There are several ASCE competitions.</h2>
-      </section>
-      <h3>All of the following regional competitions will be held from March 23, 2023 to March 25, 2023.</h3>
-      
-      
-      <section>
-      <div>
-      <ol className = 'firstCompetitionsList'>
-      <h4>Of these competitions, the most important are:</h4>
-      <li>Concrete Canoe</li>
-      <li>Steel Bridge</li>
-      <li>Timber Strong</li>
-      <li>Sustainable Solutions</li>
-      <li>Innovation Contest</li>
-      <li>Surveying</li> 
-      <li>Construction Institute</li>
-      </ol>
-      </div>
-      <div>
-      <ol className="secondCompetitionList">
-      <h5>The rest are:</h5>
-      <li>Concrete Bridge</li>
-      <li>Traffic Control</li>
-      <li>Geo-Wall</li>
-      <li>Muddy Waters</li>
-      <li>Concrete Cornhole</li>
-      <li>Plans Reading</li>
-      <li>Professional Paper</li>
-      <li>T-Shirt Contest</li>
-      <li>Mystery Competition</li>
-      </ol>
-      </div>
-      </section>
-
-      <section>
-      
-      </section>
-
-}</div>*/
+};
+export default template;     
