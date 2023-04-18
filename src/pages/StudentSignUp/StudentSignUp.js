3772e0e4bef0f351fe8d9ae3899f56771d816326
rafasellavo <rafaeljovalles@gmail.com>
2023-04-17 22:56:37 -0400

    worked fine; now it does not

--- src/pages/StudentSignUp/StudentSignUp.js
+++ src/pages/StudentSignUp/StudentSignUp.js
@@ -1,57 +1,57 @@
 import React    from "react";
 import template from "./StudentSignUp.jsx";
 
-const form = document.getElementById('asce-sign-up');
-const email = document.getElementById('@students.pup.edu');
+// const form = document.getElementById('asce-sign-up');
+// const email = document.getElementById('@students.pup.edu');
 // const name = document.getElementById('name');
 
-form.addEventListener('submit', e =>{
-  e.preventDefault();
+// form.addEventListener('submit', e =>{
+//   e.preventDefault();
 
-  validateInputs(); 
-});
+//   validateInputs(); 
+// });
 
-const setError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector('.error');
+// const setError = (element, message) => {
+//   const inputControl = element.parentElement;
+//   const errorDisplay = inputControl.querySelector('.error');
 
-  errorDisplay.innerText = message;
-  inputControl.classList.add('error');
-  inputControl.classList.remove('success');
-}
+//   errorDisplay.innerText = message;
+//   inputControl.classList.add('error');
+//   inputControl.classList.remove('success');
+// }
 
-const setSuccess = element => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector('.error');
+// const setSuccess = element => {
+//   const inputControl = element.parentElement;
+//   const errorDisplay = inputControl.querySelector('.error');
 
-  errorDisplay.innerText = '';
-  inputControl.classList.add('success');
-  inputControl.classList.remove('error');
-}
+//   errorDisplay.innerText = '';
+//   inputControl.classList.add('success');
+//   inputControl.classList.remove('error');
+// }
 
-const isValidEmail = email => {
-  const re = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:s@]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
+// const isValidEmail = email => {
+//   const re = /^(([^<>()[\]\\.,;:\s@]+(\.[^<>()[\]\\.,;:s@]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// }
 
-const validateInputs = () => {
-  const emailValue = email.value.trim();
-  // const nameValue = name.value.trim();
+// const validateInputs = () => {
+//   const emailValue = email.value.trim();
+//   // const nameValue = name.value.trim();
 
-  // if (nameValue === '') {
-  //   setError(name, 'Name is required');
-  // } else{
-  //     setSuccess(name);
-  // }
+//   // if (nameValue === '') {
+//   //   setError(name, 'Name is required');
+//   // } else{
+//   //     setSuccess(name);
+//   // }
 
-  if (emailValue === '') {
-    setError(email, 'Email is required');
-  } else if (!isValidEmail(emailValue)) {
-    setError(email, 'Provide a valid email adress');
-  } else{
-    setSuccess(email);
-  }
-}
+//   if (emailValue === '') {
+//     setError(email, 'Email is required');
+//   } else if (!isValidEmail(emailValue)) {
+//     setError(email, 'Provide a valid email adress');
+//   } else{
+//     setSuccess(email);
+//   }
+// }
 
 class StudentSignUp extends React.Component {
   render() {
