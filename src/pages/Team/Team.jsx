47e3d57f04cd1725fb03181be675091a4e9a8af2
KFGR <figueroa.8254@gmail.com>
2023-04-21 11:33:12 -0400

    deleted all components.js

--- src/pages/Team/Team.jsx
+++ src/pages/Team/Team.jsx
@@ -1,11 +1,51 @@
 import "./Team.css";
-import React from "react";
+import React, {useState} from "react";
 
-function template() {
+function Template() {
+  const [radiobuttons, setradiobuttons] = useState({
+    radiobutton1: null,
+    radiobutton2: null
+  });
+  // const [algo, setalgo] = useState("NADA");
+  // const [algo, setalgo] = useState({});
+  // const [count, setCount] = useState(0);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(radiobuttons);
+  }
+
   return (
     <div className="team">
+      <form onSubmit={handleSubmit} >
+      <input
+          type="radio"
+          name="RadioSetOne"
+          id="ascememberyes"
+          value="Yes"
+          onChange={() =>
+            setradiobuttons({
+              ...radiobuttons,
+              radiobutton1: "Yes"
+            })
+          }
+        />
+        <input
+          type="radio"
+          name="RadioSetOne"
+          id="ascememberno"
+          value="No"
+          onChange={() =>
+            setradiobuttons({
+              ...radiobuttons,
+              radiobutton1: "No"
+            })
+          }
+        />
+        <input type="submit"/>
+      </form>
     </div>
   );
 };
 
-export default template;
+export default Template;
