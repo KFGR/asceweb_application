28f5d3e05fef4f901644354dabf9c8543cff323e
rafasellavo <rafaeljovalles@gmail.com>
2023-04-22 16:25:56 -0400

    fixed spacing

--- src/pages/StudentSignUp/StudentSignUp.jsx
+++ src/pages/StudentSignUp/StudentSignUp.jsx
@@ -96,113 +96,113 @@ function Template() {
   // ---------------------------------------------------------------
 
   return (
-  <div class="container">
-	<header class="header">
-		<h1 id="title" class="text-center">Welcome to the Polytechnic University of Puerto Rico's American Society of Civil Engineers Sign Up!</h1>
-		<p id="description" class="text-center">
-      To be a part of the Polytechnic University of Puerto Rico's American Society of Civil Engineers (PUPR-ASCE), you must complete the information below!
-		</p>
-	</header>
-	<div class="form-wrap">	
-		<form id="asce-form">
-			<div class="row">
-				<div class="col-md-6">
-					<div class="form-group">
-            {/* 1. Name */}
-						<label id="name-label" for="name">Name</label>
-						<input type="text" name="fullName" id="name" placeholder="Enter your full name" class="form-control" required/>
-					</div>
-				</div>
+    <div class="container">
+    <header class="header">
+      <h1 id="title" class="text-center">Welcome to the Polytechnic University of Puerto Rico's American Society of Civil Engineers Sign Up!</h1>
+      <p id="description" class="text-center">
+        To be a part of the Polytechnic University of Puerto Rico's American Society of Civil Engineers (PUPR-ASCE), you must complete the information below!
+      </p>
+    </header>
+    <div class="form-wrap">	
+      <form id="asce-form">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              {/* 1. Name */}
+              <label id="name-label" for="name">Name</label>
+              <input type="text" name="fullName" id="name" placeholder="Enter your full name" class="form-control" required/>
+            </div>
+          </div>
 
-        {/* 2. Institutional Email */}
-				<div class="col-md-6">
-					<div class="form-group">
-						<label id="email-label" for="email">Institutional Email</label>
-						<input type="email" name="email" id="uniEmail" placeholder="Enter your email with (@students.pupr.edu)" class="form-control" required/>
-					</div>
-				</div>
-			</div>
-			
-			<div class="row">
-				<div class="col-md-6">
-					<div class="form-group">
-            {/* 3. Phone */}
-						<label id="number-label" for="number">Phone</label>
-						<input type="text" name="phone" id="phone" class="form-control" placeholder="Enter your phone number" required/>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="form-group">
-            {/* 4. shirt Size */}
-						<label>Shirt Size</label>
-						<select id="size" name="sizeShirt" class="form-control" required>
-							<option disabled selected value>Select</option>
-							<option value="small">Small</option>
-							<option value="medium">Medium</option>
-							<option value="large">Large</option>
-							<option value="extraLarge">Extra Large</option>
-						</select>
-					</div>
-				</div>
-			</div>
-			
-			<div class="row">
-				<div class="col-md-6">
-					<div class="form-group">
-          <div class="form-group">
-            {/* 5. Age */}
-						<label id="number-label" for="number">Age</label>
-						<input type="number" name="age" id="number" min="10" max="99" class="form-control" placeholder="Enter your age" required/>
-					</div>
-          <div class="form-group">
-						<label id="number-label" for="number">Department</label>
-						<input type="text" name="department" id="department" class="form-control" placeholder="Enter your department" required/>
-					</div>
-					</div>
-				</div>
+          {/* 2. Institutional Email */}
+          <div class="col-md-6">
+            <div class="form-group">
+              <label id="email-label" for="email">Institutional Email</label>
+              <input type="email" name="email" id="uniEmail" placeholder="Enter your email with (@students.pupr.edu)" class="form-control" required/>
+            </div>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              {/* 3. Phone */}
+              <label id="number-label" for="number">Phone</label>
+              <input type="text" name="phone" id="phone" class="form-control" placeholder="Enter your phone number" required/>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              {/* 4. shirt Size */}
+              <label>Shirt Size</label>
+              <select id="size" name="sizeShirt" class="form-control" required>
+                <option disabled selected value>Select</option>
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+                <option value="extraLarge">Extra Large</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+            <div class="form-group">
+              {/* 5. Age */}
+              <label id="number-label" for="number">Age</label>
+              <input type="number" name="age" id="number" min="10" max="99" class="form-control" placeholder="Enter your age" required/>
+            </div>
+            <div class="form-group">
+              <label id="number-label" for="number">Department</label>
+              <input type="text" name="department" id="department" class="form-control" placeholder="Enter your department" required/>
+            </div>
+            </div>
+          </div>
 
-				<div class="col-md-6">
-					<div class="form-group">
-          <div class="form-group">
-            {/* 6. Bachelor */}
-						<label id="number-label" for="number">Bachelor</label>
-						<input type="text" name="bachelor" id="bachelor" class="form-control" placeholder="Enter your bachelor" required/>
-					</div>
-          <div class="form-group">
-            {/* 8. Academic Year */}
-						<label id="number-label" for="number">Academic Year</label>
-						<input type="text" name="academicYear" id="academic" min="1" max="15" class="form-control" placeholder="Enter your academic year" required/>
-					</div>
-					</div>
-				</div>
-			</div>
+          <div class="col-md-6">
+            <div class="form-group">
+            <div class="form-group">
+              {/* 6. Bachelor */}
+              <label id="number-label" for="number">Bachelor</label>
+              <input type="text" name="bachelor" id="bachelor" class="form-control" placeholder="Enter your bachelor" required/>
+            </div>
+            <div class="form-group">
+              {/* 8. Academic Year */}
+              <label id="number-label" for="number">Academic Year</label>
+              <input type="text" name="academicYear" id="academic" min="1" max="15" class="form-control" placeholder="Enter your academic year" required/>
+            </div>
+            </div>
+          </div>
+        </div>
 
-			<div class="row">
-				<div class="col-md-12">
-					<div class="form-group">
-            <div class="disclaimer">
-              {/* Checkbox */}
-              <input type="checkbox" 
-                id="checkbox" required/> After the "Submit" button, there will be a fee of $25.00 dollars
-                in order to become a member of the PUPR-ASCE, by contacting the PUPR-ASCE treasuer directive member. 
-                The $25.00 dollars are not refundable.  
-              </div>   
-					</div>
-				</div>
-			</div>
-			
-			<div class="row">
-				<div class="col-12">
-          {/* Button*/} 
-					<button type="submit" id="submit" class="btn btn-primary btn-block">Submit</button>
-				</div>
-			</div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <div class="disclaimer">
+                {/* Checkbox */}
+                <input type="checkbox" 
+                  id="checkbox" required/> After the "Submit" button, there will be a fee of $25.00 dollars
+                  in order to become a member of the PUPR-ASCE, by contacting the PUPR-ASCE treasuer directive member. 
+                  The $25.00 dollars are not refundable.  
+                </div>   
+            </div>
+          </div>
+        </div>
+        
+        <div class="row">
+          <div class="col-12">
+            {/* Button*/} 
+            <button type="submit" id="submit" class="btn btn-primary btn-block">Submit</button>
+          </div>
+        </div>
 
-		</form>
-	</div>	
-</div>
+      </form>
+	  </div>	
+  </div>
 
-);
+  );
 };
 
 export default Template;
