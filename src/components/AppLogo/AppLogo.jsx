d80f3a8af64725e3650d2041c196ccfc0870427c
KFGR <figueroa.8254@gmail.com>
2023-04-24 09:39:11 -0400

    changed the logo, for the new one

--- src/components/AppLogo/AppLogo.jsx
+++ src/components/AppLogo/AppLogo.jsx
@@ -1,21 +1,25 @@
 import "./AppLogo.css";
 import React from "react";
 import Navbar from 'react-bootstrap/Navbar';
-import Icon from "./logo";
-
+// import Icon from "./logo";
+// import icon from '../../assets/Brand/logo/Logo.png';
+import Icon from './newlogo';
 function template(props) {
   const {id} = props;
   return (
-    <div className="app-logo">
+    <div className="app-logo app-logo-container">
       {id === "Header_logo" &&(
         <div className="svg-container">
-         <Navbar.Brand href='/Home'><svg><Icon/></svg></Navbar.Brand>
+        <Navbar.Brand href='/Home'><svg><Icon viewbox="0 75 804 150"/></svg></Navbar.Brand>
+        {/* <Icon viewbox="0 75 504 150" /> */}
+        {/* <Icon viewbox="0 75 804 150" /> */}
+
         </div>
         )}
 
         {id === "Footer_logo" && (
           <div className="svg-container">
-            <Icon/>
+            <Icon viewbox="0 0 504 360"/>
           </div>
         )}
       </div>
