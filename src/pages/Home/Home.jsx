446b5aea2c63917484636b84788850d7062d65d8
KFGR <figueroa.8254@gmail.com>
2023-04-25 19:21:29 -0400

    New sections and imports

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,24 +1,51 @@
 import "./Home.css";
 import Counter from "./Counter";
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHandshake, faUsers} from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faHandshake, faUsers} from "@fortawesome/free-solid-svg-icons";
 import { Image } from "react-bootstrap";
-// import HeaderHero from './HeaderHero';
+
+
 import ContentCard from '../../components/ContentCard';
-import Award from '../../assets/Brand/Award.png';
-import Members from '../../assets/Brand/members.png';
-import Gallagher from '../../assets/Brand/Gallagher3.jpg';
+import Calendar from '../../components/Calendar';
+import Carousel from "../../components/Carousel";
+
 import Image3 from '../../assets/Brand/Image3.jpeg';
 
-import image1 from '../../assets/Brand/Gallagher3.jpg';
-import image2 from '../../assets/Brand/Image2.jpeg';
-import image3 from '../../assets/Brand/Image7.jpeg';
+//Images for the Carousel
+import Gallagher from '../../assets/Carousel_images/Gallagher3.jpg';
+// import image1 from '../../assets/Carousel_images/Gallagher2.jpeg';
+// import image6 from '../../assets/Carousel_images/Image6.jpeg';
+// import image2 from '../../assets/Carousel_images/Image2.jpeg';
+
+// import image3 from '../../assets/Carousel_images/Image7.jpeg';
+// import image4 from '../../assets/Carousel_images/Image4.jpeg';
+// import image5 from '../../assets/Carousel_images/Image5.jpeg';
+
+// import image8 from '../../assets/Carousel_images/image8.jpeg';
+// import image9 from '../../assets/Carousel_images/image9.jpeg';
+
+//newones
+import first from '../../assets/Carousel_images/1.png';
+import second from '../../assets/Carousel_images/2.png';
+import third from '../../assets/Carousel_images/3.png';
+
+import fourth from '../../assets/Carousel_images/4.png';
+import fifth from '../../assets/Carousel_images/5.png';
+import sixth from '../../assets/Carousel_images/6.png';
+
+import seventh from '../../assets/Carousel_images/7.png';
+import eight from '../../assets/Carousel_images/8.png';
+import nine from '../../assets/Carousel_images/9.png';
+
+import ten from '../../assets/Carousel_images/10.png';
+import eleven from '../../assets/Carousel_images/11.png';
+import twelve from '../../assets/Carousel_images/12.png';
 
 
-import Calendar from '../../components/Calendar';
-// import Carousel from '../../components/Carousel';
-import Carousel from "../../components/Carousel";
+
+
+
 
 
 function template (){
@@ -27,7 +54,8 @@ function template (){
     <div className="home">
       {/* Section divider */}
       <div className="container-fluid py-2 header-yllw"/>
-
+      
+      {/* Section for cards */}
       <section id="About">
 
       {/* About us */}
@@ -52,82 +80,11 @@ function template (){
       />
     </section>
 
-    <div className="container-fluid py-2 header-yllw"/> 
-
-
-  
-       {/* Section divider */}
-       <div className="container-fluid py-2 header-finite-ble"/>
-
-      
-        <div className="container-fluid container-values">
-          <div className="container">
-            <h1 className='h1-text-content' id='mission_txt' style={{color:'black', textAlign:'center'}}><strong>Mission and Values </strong></h1>
-            <div className="row ">
-              <div className="col-12">
-                <div className="row row-values">
-                  <div className="col-lg-6">
-                      <div className="grid-item move-up">
-                        <div className="ht-box-icon">
-                          <div className="icon-box-wrap">
-                            <div className="content-header">
-                              <div className="icon">
-                                <FontAwesomeIcon icon={faHandshake}  />
-                              </div>
-                              <h5 className="heading" style={{color:'black'}}>Mission</h5>
-                            </div>
-                            <div className="content">
-                              <h4 style={{color:'black'}}>
-                              To inspire and expose all our members to new experiences and create a space where 
-                              they can develop themselves professionally and enrich them through a competitive culture.
-                              </h4>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  <div className="col-lg-6">
-                    <div className="grid-item move-up">
-                      <div className="ht-box-icon">
-                        <div className="icon-box-wrap">
-                          <div className="content-header">
-                            <div className="icon">
-                              <FontAwesomeIcon icon={faUsers} />
-                            </div>
-                            <h5 className="heading" style={{color:'black'}}>Vision</h5>
-                          </div>
-                          <div className="content">
-                            <h4 style={{color:'black'}}>
-                            Lead all our members through the experiences the gather to have better tools for them to increase their chances at better opportunities.
-                            </h4>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  
-                </div>
-              </div>
-            </div>
-        </div>
-      </div>
-      <div className="container-fluid py-2 header-finite-ble"/>
        <div className="container-fluid py-2 header-yllw"/>
 
-       <section className="container-fluid">
-       <div className="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3" style={{justifyContent:"space-evenly"}}>
-            <div className="col-lg-6 p-3 p-lg-5 pt-lg-3">
-            <Carousel image1={image1} image2={image2} image3={image3}/>
-            </div>
-            <div className="col-lg-5 offset-lg-1 p-0  shadow-lg">
-            <Carousel image1={image1} image2={image2} image3={image3}/>            </div>
-        </div>
-        
-
-       </section>
-
-      <section id="Counter" className="counts">
-      <div className="container-fluid" style={{padding:'1rem'}}>
+        {/* Section to display the ammount of members, years and sponsors */}
+       <section id="Counter" className="counts">
+      <div className="container-fluid" style={{paddingTop:'2rem'}}>
         <div className="container-fluid">
           <h1 className='h1-text-content-chapter' id='mission_txt' style={{color:'black', textAlign:'center'}}><strong>Our Chapter</strong></h1>
         <div className="row counters">
@@ -150,15 +107,50 @@ function template (){
         </div>
       </div>
     </section>
+
+        {/* Section to display images about the chapter,  */}
+       <section className="container-fluid">
+       <div className="row p-4 pb-0 pe-lg-0 align-items-center rounded-3" style={{justifyContent:"space-evenly"}}>
+            <div className="col-lg-6 column-padding" style={{margin:'auto'}}>
+              <Carousel image1={first} image2={second} image3={third}/>
+            </div>
+            <div className="col-lg-6 column-padding" style={{margin:'auto'}}>
+              <Carousel image1={fourth} image2={fifth} image3={sixth}/>
+            </div>
+        </div>
+        <div className="row p-4 pb-0 pe-lg-0 align-items-center rounded-3" style={{justifyContent:"space-evenly"}}>
+            <div className="col-lg-6 column-padding" style={{margin:'auto'}}>
+              <Carousel image1={seventh} image2={eight} image3={nine}/>
+            </div>
+            <div className="col-lg-6 column-padding" style={{margin:'auto'}}>
+              <Carousel image1={ten} image2={eleven} image3={twelve}/>
+            </div>
+        </div>
+       </section>
+
+
     {/* Section divider */}
     <div className="container-fluid py-2 header-finite-ble"/> 
     <div className="container-fluid py-2 header-yllw"/> 
-    
+
+    <section id="calendar_activities" className="container-fluid home-calendar">
+    <div className="container-fluid" style={{paddingTop:'2rem', paddingBottom:'2rem'}}>
+        <div className="container-fluid">
+          <h1 className='h1-text-content-calendar' id='mission_txt' style={{color:'black', textAlign:'center'}}><strong>Our Chapter</strong></h1>
+          <div className="row home-calendar">
+            <Calendar/>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div className="container-fluid py-2 header-finite-ble"/> 
+    <div className="container-fluid py-2 header-yllw"/> 
+
     <div className="container container-content">
       <div>
         <h1 className='h1-text-content' id='mission_txt' style={{color:'black', textAlign:'center'}}><strong>Our Sponsors</strong></h1>
-      <Image src={require("../../assets/Brand/clients.png")} alt="" width={500} height={500} style={{width:'100%', height:'auto'}}/>
-      {/* <ImageModal show={showModal} onHide={handleCloseModal}/> */}
+        <Image src={require("../../assets/Brand/clients.png")} alt="" width={500} height={500} style={{width:'100%', height:'auto'}}/>
       </div>
     </div>
 </div>
