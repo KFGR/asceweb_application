b6f3554bc07b166617850acd0319d92f73683145
KFGR <figueroa.8254@gmail.com>
2023-04-25 20:11:26 -0400

    Changes to counter, when user is in viewY(1000)

--- src/pages/Home/Counter.jsx
+++ src/pages/Home/Counter.jsx
@@ -2,16 +2,32 @@ import React, { useState, useEffect } from "react";
 
 function Counter(props) {
   const [count, setCount] = useState(props.start);
+  const [view, setView] = useState(false);
 
   useEffect(() => {
+    const Scroll = () => {
+      if (window.scrollY > 1000){
+        setView(true);
+      }
+    };
+    window.addEventListener("scroll", Scroll);
+
+    return () => {
+      window.removeEventListener("scroll", Scroll);
+    };
+
+  }, []);
+
+  useEffect(() => {
+    if (view){
     const id = setInterval(() => {
       if (count < props.end) {
         setCount(count + 1);
       }
     }, 100);
-
     return () => clearInterval(id);
-  }, [count, props.end]);
+  }
+  }, [view,count, props.end]);
 
   return (
     <div className="counter">
