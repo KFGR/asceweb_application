c4d14a20f0fb6036bfe9251e4b9c2d5b76a2b4a4
rafasellavo <rafaeljovalles@gmail.com>
2023-05-01 12:31:06 -0400

    email problem fied :)

--- src/pages/StudentSignUp/StudentSignUp.jsx
+++ src/pages/StudentSignUp/StudentSignUp.jsx
@@ -1,99 +1,56 @@
 import "./StudentSignUp.css";
-import React  from "react";
-// import{ useState } from "react"
-
-//TO-DO:
-//find a way to validate inputs, in also fixing the 'white page' problem
-//find a way to have it more centered that it already is
-//add checkbox
-
-  //email validation @pupr.edu || @students.pupr.edu 
-  //label for = form-email className=form-label /label (inside input) 
-
-// const [submitted, setSubmitted] = useState();
-// const [valid, setValid] = useState();
-
+import React, { useState } from "react";
+ 
 function Template() {
 
-  // ---------------------------------------------------------------
   // validation form
-  // const [values, setValues] = useState({
-  //   name: "",
-  //   email: "",
-  //   phone: "",
-  //   size: "",
-  //   age: "",
-  //   bachelor: "",
-  //   department: "",
-  //   academic: ""
-  // });
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    size: '',
+    age: '',
+    bachelor: '',
+    department: '',
+    academic: ''
+  });
 
-  // const handleInputChange = (event) => {
-  //   event.preventDefault();
+  //checkInput function for the validation 
+  function checkInput (formData) {
+    //validation constants
+    const regexEmail = /^[a-zA-Z]+_[0-9]+@students\.pupr\.edu$/;
+    let hasError = false;
+    
+    if (!regexEmail.test(formData.email)){
+      alert('Please enter a @students.pupr.edu valid email!');
+      hasError = true;
+    }
 
-  //   const {name, value} = event.target;
-  //   setValues((values) => ({
-  //     ...values,
-  //     [name]: value
-  //   }));
+    return(!hasError);  
+  };
+    
+  const handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
 
-  //       // const [submitted, setSubmitted] = useState(false);
-  //       // const [valid, setValid] = useState(true);
-  //   };
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
   
-  // const [submitted, setSubmitted] = useState(false);
-  // const [valid, setValid] = useState(true);
-
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     if (values.fullName && values.email && values.phone && values.shirtSize
-  //         && values.age && values.bachelor && values.department && values.academicYear) {
-  //           setValid(true);
-  //       }
-  //       setSubmitted(true);
-  //   };
-
-  //   //checkbox
-  //   const [checked, setChecked] = useState(false);
-
-  //   const handleChange = () => {
-  //     setChecked(!checked);
-  //   };
-  // ---------------------------------------------------------------
-
-  // ---------------------------------------------------------------
-  //JSON format
-  // const form = document.getElementById("asce-form");
-
-  // form.addEventListener("submit", function (event) {
-  //   event.preventDefault();
-
-  //   const name = document.getElementById("name").value;
-  //   const email = document.getElementById("email").value;
-  //   const phone = document.getElementById("phone").value;
-  //   const size = document.getElementById("shirtSize").value;
-  //   const age = document.getElementById ("age").value;
-  //   const bachelor = document.getElementById("bachelor").value;
-  //   const department = document.getElementById("department").value;
-  //   const academic = document.getElementById("academic").value;
-
-  //   const formData = {
-  //     name: name,
-  //     email: email,
-  //     cellphone: phone,
-  //     size: size,
-  //     age: age,
-  //     degree: bachelor,
-  //     department: department,
-  //     academic: academic
-  //   }
-
-  //   console.log(formData);
-
-  //   const formDataJson = JSON.stringify(formData);
-  //   console.log(formDataJson); 
-  // });
-  // ---------------------------------------------------------------
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+    if (checkInput(formData)){      
+      const formDataJson = JSON.stringify(formData);
+      console.log(formDataJson);
+    }else {
+      console.log('error');
+      alert('Input ERROR');
+      }
+  };
 
   return (
     <div class="container">
@@ -104,13 +61,13 @@ function Template() {
       </p>
     </header>
     <div class="form-wrap">	
-      <form id="asce-form">
+      <form id="asce-form" onSubmit={handleSubmit}>
         <div class="row">
           <div class="col-md-6">
             <div class="form-group">
               {/* 1. Name */}
-              <label id="name-label" for="name">Name</label>
-              <input type="text" name="fullName" id="name" placeholder="Enter your full name" class="form-control" required/>
+              <label id="name-label" for="name">First & Last Name</label>
+              <input type="text" name="name" id="name" placeholder="John Doe" class="form-control" onChange={handleChange} required/>
             </div>
           </div>
 
@@ -118,7 +75,7 @@ function Template() {
           <div class="col-md-6">
             <div class="form-group">
               <label id="email-label" for="email">Institutional Email</label>
-              <input type="email" name="email" id="uniEmail" placeholder="Enter your email with (@students.pupr.edu)" class="form-control" required/>
+              <input type="email" name="email" id="uniEmail" placeholder="doe_123456@students.pupr.edu" class="form-control" onChange={handleChange} required/>
             </div>
           </div>
         </div>
@@ -128,14 +85,14 @@ function Template() {
             <div class="form-group">
               {/* 3. Phone */}
               <label id="number-label" for="number">Phone</label>
-              <input type="text" name="phone" id="phone" class="form-control" placeholder="Enter your phone number" required/>
+              <input type="text" name="phone" id="phone" class="form-control" placeholder="787-123-4567" onChange={handleChange} required/>
             </div>
           </div>
           <div class="col-md-6">
             <div class="form-group">
               {/* 4. shirt Size */}
               <label>Shirt Size</label>
-              <select id="size" name="sizeShirt" class="form-control" required>
+              <select id="size" name="size" class="form-control" onChange={handleChange} required>
                 <option disabled selected value>Select</option>
                 <option value="small">Small</option>
                 <option value="medium">Medium</option>
@@ -152,11 +109,11 @@ function Template() {
             <div class="form-group">
               {/* 5. Age */}
               <label id="number-label" for="number">Age</label>
-              <input type="number" name="age" id="number" min="10" max="99" class="form-control" placeholder="Enter your age" required/>
+              <input type="text" name="age" id="number" class="form-control" placeholder="Enter your age" onChange={handleChange} required/>
             </div>
             <div class="form-group">
               <label id="number-label" for="number">Department</label>
-              <input type="text" name="department" id="department" class="form-control" placeholder="Enter your department" required/>
+              <input type="text" name="department" id="department" class="form-control" placeholder="Enter your department" onChange={handleChange} required/>
             </div>
             </div>
           </div>
@@ -166,12 +123,12 @@ function Template() {
             <div class="form-group">
               {/* 6. Bachelor */}
               <label id="number-label" for="number">Bachelor</label>
-              <input type="text" name="bachelor" id="bachelor" class="form-control" placeholder="Enter your bachelor" required/>
+              <input type="text" name="bachelor" id="bachelor" class="form-control" placeholder="Enter your bachelor" onChange={handleChange} required/>
             </div>
             <div class="form-group">
               {/* 8. Academic Year */}
               <label id="number-label" for="number">Academic Year</label>
-              <input type="text" name="academicYear" id="academic" min="1" max="15" class="form-control" placeholder="Enter your academic year" required/>
+              <input type="text" name="academic" id="academic" min="1" max="15" class="form-control" placeholder="Enter your academic year" onChange={handleChange} required/>
             </div>
             </div>
           </div>
